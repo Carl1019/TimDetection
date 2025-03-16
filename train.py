d3bf5fce7038926d6425e474136d13af3628627c
haoxiang Yuan <h63yuan@waterloo.ca>
2025-03-16 16:36:16 -0400

    WANDB on train.py

--- train.py
+++ train.py
@@ -1,9 +1,10 @@
 from ultralytics import YOLO
 import os
 import time
+import wandb 
 
-# 暂时禁用 wandb
-os.environ["WANDB_DISABLED"] = "true"
+# 初始化 wandb
+wandb.init(project="YOLO_Tim", name="first training")
 
 def train_yolo():
     # 加载 YOLOv8 预训练模型
@@ -12,7 +13,7 @@ def train_yolo():
     # 训练参数
     model.train(
         data="Dataset/data.yaml",  
-        epochs=50,
+        epochs=5, #测试用的话把这个调小
         batch=16,
         imgsz=640,
         optimizer="SGD",
@@ -20,8 +21,11 @@ def train_yolo():
         lrf=0.001,
         weight_decay=0.0005,
         dropout=0.1,
-        device="cuda"  # 使用我自己的GPU
+        device="cuda",  # 使用 GPU
+        project="runs/WB_experiment",  
+        name="first training",
     )
+
 if __name__ == "__main__":
     start_time = time.time()
     train_yolo()
@@ -29,3 +33,8 @@ if __name__ == "__main__":
     elapsed_time = end_time - start_time
     print(f"Time spend on Training: {elapsed_time:.2f} second")
 
+    # 记录训练时间到 W&B
+    wandb.log({"Training Time": elapsed_time})
+
+    # 结束 W&B 运行
+    wandb.finish()
