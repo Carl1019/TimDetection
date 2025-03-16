454dd84910d0fb2d80f0e73b01a22aca1609789f
haoxiang Yuan <h63yuan@waterloo.ca>
2025-03-16 09:31:20 -0400

    sync file name

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ from ultralytics import YOLO
 
 def test_yolo():
     # 加载训练完成的模型
-    model = YOLO(r"D:\SYDE770 Project\TimDetection\runs\detect\train2\weights\best.pt")
+    model = YOLO(r"D:\SYDE770 Project\TimDetection\runs\detect\First_success_tranining\weights\best.pt")
     # 在测试集上评估模型
     metrics = model.val(data="D:/SYDE770 Project/TimDetection/Dataset/data.yaml", split="test")
     print(metrics)  # 打印评估结果
